c2c3cbfcb6fa56f58f1817bdcff70a3b57c4cf6c
lipeng <17314429@qq.com>
2016-05-03 10:52:42 +0800

    collection view 例子补充自定义数据情况

--- Workspace/TestCollectionViewController.m
+++ Workspace/TestCollectionViewController.m
@@ -15,6 +15,7 @@
 @interface TestCollectionViewController ()
 
 @property(nonatomic, strong) ComCollectionView *collectionView;
+@property(nonatomic, strong) NSArray *dataArray;
 
 @end
 
@@ -40,19 +41,20 @@
         
         _collectionView.listModel = [[BookListModel alloc] init];
         _collectionView.cellClass = [BookCollectionViewCell class];
-//        _collectionView.cellConfigureBlock = ^(UICollectionViewCell *cell, BookModel *data, NSIndexPath *indexPath) {
-//            cell.backgroundColor = [UIColor colorWithRed:((10 * indexPath.row) / 255.0) green:((20 * indexPath.row)/255.0) blue:((30 * indexPath.row)/255.0) alpha:1.0f];
-//            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
-//            label.textColor = [UIColor redColor];
-//            label.text = [NSString stringWithFormat:@"%zd",indexPath.row];
+//        _collectionView.dataArray = self.dataArray;
+//        _collectionView.cellConfigureBlock = ^(UICollectionViewCell *cell, NSDictionary *data, NSIndexPath *indexPath) {
+//            cell.backgroundColor = [UIColor purpleColor];
+//            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 80, 50)];
+//            label.textColor = [UIColor whiteColor];
+//            label.font = [UIFont systemFontOfSize:14];
+//            label.numberOfLines = 0;
+//            label.text = [NSString stringWithFormat:@"%@", data[@"vcName"]];
 //            
-//            for (id subView in cell.contentView.subviews) {
-//                [subView removeFromSuperview];
-//            }
+//            [cell.contentView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
 //            [cell.contentView addSubview:label];
 //        };
 //        _collectionView.isPaging = NO;
-        _collectionView.isRefresh = NO;
+//        _collectionView.isRefresh = NO;
         _collectionView.cellSelectBlock = ^(UICollectionView *tableView, NSIndexPath *indexPath) {
             
         };
@@ -60,34 +62,15 @@
     return _collectionView;
 }
 
-//定义每个Item 的大小
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    CGFloat width = (collectionView.width)/2.0-5;
-    CGFloat height = width + 50.0;
-    
-    return CGSizeMake(width, height);
-}
-
-//定义每个UICollectionView 的 margin
--(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
-{
-    return UIEdgeInsetsZero;
-//    return UIEdgeInsetsMake(0, 5, 5, 5);
-}
-
-//UICollectionView被选中时调用的方法
--(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewCell * cell = (UICollectionViewCell *)[collectionView cellForItemAtIndexPath:indexPath];
-    //临时改变个颜色，看好，只是临时改变的。如果要永久改变，可以先改数据源，然后在cellForItemAtIndexPath中控制。（和UITableView差不多吧！O(∩_∩)O~）
-    cell.backgroundColor = [UIColor greenColor];
-}
-
-//返回这个UICollectionView是否可以被选择
--(BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    return YES;
+- (NSArray *)dataArray {
+    if (!_dataArray) {
+        _dataArray = @[@{@"title":@"TableView", @"vcName":@"TestTableViewController"},
+                       @{@"title":@"CollectionView", @"vcName":@"TestCollectionViewController"},
+                       @{@"title":@"CollectionView", @"vcName":@"TestCollectionViewController"},
+                       @{@"title":@"CollectionView", @"vcName":@"TestCollectionViewController"},
+                       @{@"title":@"ScrollView", @"vcName":@"TestScrollViewController"}];
+    }
+    return _dataArray;
 }
 
 - (void)didReceiveMemoryWarning {
