3d48a58e8e4bc9e1559248a5e7b02867e5ffafbf
lipeng <17314429@qq.com>
2016-05-03 17:13:27 +0800

    color

--- Workspace/Define/ComUIDefine.h
+++ Workspace/Define/ComUIDefine.h
@@ -10,12 +10,9 @@
 #define APP_SCREEN_WIDTH     [UIScreen mainScreen].bounds.size.width
 #define APP_SCREEN_HEIGHT    [UIScreen mainScreen].bounds.size.height
 
-////-----------------------------------  颜色  ------------------------------------
-//// e.g. HEXCOLOR(0xCECECE);
-//#define HEXCOLOR(rgbValue)  [TBCityColorManager colorWithHex:rgbValue alpha:1.f]
-//
-//// e.g. HEXCOLORA(0xCECECE, 0.8);
-//#define HEXCOLORA(rgbValue,a) [TBCityColorManager colorWithHex:rgbValue alpha:a]
+//-----------------------------------  颜色  ------------------------------------
+#define HEXCOLOR(rgbValue)  [ComColorManager colorWithHex:rgbValue alpha:1.f]
+#define HEXCOLORA(rgbValue,a) [ComColorManager colorWithHex:rgbValue alpha:a]
 
 //-----------------------------------  旋转角度  ------------------------------------
 #define DegreesToRadians(degrees)             degrees*M_PI/180
